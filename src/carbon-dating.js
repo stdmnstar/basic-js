26a98c9936cc3cede3d7b078f3ff3e079d544ed4
Aliaksandr Tarhonski <stdmnstar@gmail.com>
2020-09-26 01:18:10 +0300

    feat: solution of count cats, carbon dation, dream team

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+
+  if (typeof sampleActivity != 'string' || isNaN(+sampleActivity)) {
+    return false;
+  }
+  if (+sampleActivity <= 0 || +sampleActivity > MODERN_ACTIVITY) {
+    return false;
+  }
+  return Math.ceil(Math.log(MODERN_ACTIVITY / +sampleActivity) / (0.693 / HALF_LIFE_PERIOD));
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let teamName = [];
+
+  if (!Array.isArray(members)) {
+    return false;
+  }
+
+  for (let i = 0; i < members.length; i++) {
+    if (typeof members[i] != "string") {
+      continue;
+    }
+    teamName.push(members[i].trim()[0].toUpperCase());
+  }
+
+  return teamName.sort().join('');
 };
