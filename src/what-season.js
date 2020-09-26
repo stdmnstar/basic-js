00915396fb3d44d86dcb054ba09909b6884f92c0
Aliaksandr Tarhonski <stdmnstar@gmail.com>
2020-09-26 09:33:46 +0300

    feat: soiution what season, hanoi tower

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,23 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (!date) {
+    return "Unable to determine the time of year!";
+  }
+  if (Object.prototype.toString.call(date) !== '[object Date]') {
+    throw Error();
+  }
+  const month = date.getMonth() + 1;
+  if (month <= 2 || month == 12) {
+    return 'winter';
+  }
+  if (month >= 3 && month <= 5) {
+    return 'spring';
+  }
+  if (month >= 6 && month <= 8) {
+    return 'summer';
+  }
+  if (month >= 9 && month <= 11) {
+    return 'autumn';
+  }
 };
