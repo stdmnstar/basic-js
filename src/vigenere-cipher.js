7dab8ea4934e57129ee763fbc88ddbf2b8a12e84
Aliaksandr Tarhonski <stdmnstar@gmail.com>
2020-09-27 13:12:00 +0300

    feat: solution vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,59 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = direct;
   }
+
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Error');
+    }
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const arr = [];
+    let keyStep = -1;
+    for (let i = 0; i < message.length; i++) {
+      if (alphabet.indexOf(message[i].toUpperCase()) > -1) {
+        keyStep++;
+        let number = (alphabet.indexOf(message[i].toUpperCase()) + alphabet.indexOf(key[keyStep % key.length].toUpperCase())) % alphabet.length;
+        arr.push(alphabet[number]);
+      } else {
+        arr.push(message[i]);
+      }
+    }    
+    if (this.direct) {
+      return arr.join('');
+    } else {
+      return arr.reverse().join('');
+    }
+  }
+  decrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Error');
+    }
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const arr = [];
+    let keyStep = -1;
+    for (let i = 0; i < message.length; i++) {
+      if (alphabet.indexOf(message[i].toUpperCase()) > -1) {
+        keyStep++;
+        let number = (alphabet.indexOf(message[i].toUpperCase()) + alphabet.length - alphabet.indexOf(key[keyStep % key.length].toUpperCase())) % alphabet.length;
+        arr.push(alphabet[number]);
+      } else {
+        arr.push(message[i]);
+      }
+    }
+    if (this.direct) {
+      return arr.join('');
+    } else {
+      return arr.reverse().join('');
+    }
+  }
+
+
+
+
 }
 
 module.exports = VigenereCipheringMachine;
